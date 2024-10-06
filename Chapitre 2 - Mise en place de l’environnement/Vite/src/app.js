4771263ffede88f9436c507e26bd7d1e3a13c0b8
Dylane <tanolamene@gmail.com>
2024-10-06 09:06:25 -0300

    Mise a jour

--- Chapitre 2 - Mise en place de l’environnement/Vite/src/app.js
+++ Chapitre 2 - Mise en place de l’environnement/Vite/src/app.js
@@ -23,3 +23,27 @@ console.log("Hello World !");
   }
 }
  */
+
+console.info("test");
+console.warn("test");
+console.error("test");
+console.log("test");
+
+// On peut afficher le temps passé entre l'exécution de la ligne suivante
+// et de l'exécution de la ligne d'après :
+console.time();
+console.timeEnd();
+
+// Affichage d'un tableau, nous verrons en détails les tableaux dans les
+// leçons suivantes :
+console.table(["chat", "chien", "koala"]);
+
+// Affichera une erreur pour le deuxième car l'assertion est fausse :
+console.assert(true, "Bonjour 1 !");
+console.assert(false, "Bonjour 2 !");
+
+// Compter le nombre d'exécution :
+// Nous étudierons les boucles en détails plus tard également.
+for (let i = 0; i < 10; i++) {
+  console.count();
+}
