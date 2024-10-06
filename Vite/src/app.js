f0e243dc4191069aaf5f2b00b94ae05a6723af1f
Dylane <tanolamene@gmail.com>
2024-10-06 08:40:27 -0300

    Installation et configuration de vite

---
+++ Vite/src/app.js
@@ -0,0 +1,25 @@
+console.log("Hello World !");
+
+
+/**
+ * Package.json
+  {
+  "name": "vite",
+  "version": "1.0.0",
+  "description": "",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "vite",  // Stater un projet vite
+    "build": "vite build",   // creer un dossier dist(distrubution l'app est terminer)
+    "preview": "vite preview",  // Previsualiser l'applocation final
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "vite": "^5.4.8"
+  }
+}
+ */
