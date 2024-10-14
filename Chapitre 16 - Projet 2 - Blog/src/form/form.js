fc5f5d1a9dc75db9b8992d66ae1bfcffc3bfd5f4
Dylane <tanolamene@gmail.com>
2024-10-14 16:03:38 -0300

    MAJ

--- Chapitre 16 - Projet 2 - Blog/src/form/form.js
+++ Chapitre 16 - Projet 2 - Blog/src/form/form.js
@@ -1,13 +1,54 @@
-import "../assets/styles/styles.scss"
+import "../assets/styles/styles.scss";
 import "./form.scss";
 
 const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors")
+let errors = [];
 
-
-form.addEventListener("submit",e => {
+form.addEventListener("submit",async (e) => {
   e.preventDefault();
   const formData = new FormData(form);
-  const entries = formData.entries()
-  const json = JSON.stringify(Object.fromEntries(entries))
-  console.log(json);
-})
+  const entries = formData.entries();
+  const article = Object.fromEntries(entries);
+  console.log(article);
+  
+  if (formIsValid(article)) {
+    try{
+
+      const json = JSON.stringify(article);
+      const res = await fetch("https://restapi.fr/api/article",{
+        method:"POST",
+        body:json,
+        headers: {
+          "Content-Type":"application/json"
+        }
+      }); 
+      const body = await res.json();
+      console.log(body);
+      
+    }
+    catch(e){
+      console.error("e : ",e)
+    }
+  }
+});
+
+const formIsValid = (article) => {
+  if(!article.author || !article.categorie || !article.article){
+    errors.push("Vous devez renseigner tous les champs")
+  }else{
+    errors = [];
+  }
+  if(errors.length){
+    let errorHtmL = "";
+    errors.forEach((e => {
+      errorHtmL += `<li>${e}</li>`;
+    }))
+    errorElement.innerHTML = errorHtmL;
+    return false;
+  }else{
+    errorElement.innerHTML = '';
+    return true
+  }
+};
+console.log(errorElement);
--- Chapitre 16 - Projet 2 - Blog/src/main.js
+++ Chapitre 16 - Projet 2 - Blog/src/main.js
@@ -1,5 +1,37 @@
 import "./assets/styles/styles.scss";
 import "./index.scss"
-import "./assets/javacript/topbar"
 
-console.log("In Accueil");
+const articleContainerElement = document.querySelector(".articles-container");
+const createArticle = (articles) => {
+const articlesDOM = articles.map(article => {
+  const articleDOM = document.createElement('div');
+  articleDOM.classList.add("article");
+  articleDOM.innerHTML = `
+  <img src="${article.img}" alt="profile">
+  <h2>titre de l'article</h2>
+  <p class="article-author">${article.author} - ${article.categorie}</p>
+  <p class="article-content">
+  ${article.article}
+  </p>
+  <div class="article-actions">
+    <button class="btn btn-primary">Modifier</button>
+    <button class="btn btn-secondary" data-id="${article._id}">Supprimer</button>
+  </div>
+  `;
+  return articleDOM;  
+});
+articleContainerElement.innerHTML = '';
+articleContainerElement.append(...articlesDOM);
+
+}
+const fetchArticle = async () => {
+  try{
+    const response = await fetch("https://restapi.fr/api/article");
+    const articles = await response.json();
+    console.log(articles);
+    createArticle(articles);
+  }catch(e){
+    console.error("e : ",e)
+  }
+}
+fetchArticle();
